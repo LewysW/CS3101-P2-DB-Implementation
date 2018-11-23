dbfbd19731f6e5bbbaaa542711a7b5723fa3aa52
locw <lewysward99@gmail.com>
2018-11-23 22:40:02 +0000

    Finished creation of database

--- schema.sql
+++ schema.sql
@@ -1,19 +1,16 @@
-DROP DATABASE [IF EXISTS] locw_bookstream;
+DROP DATABASE IF EXISTS locw_bookstream;
 
 CREATE DATABASE locw_bookstream;
 
 USE locw_bookstream;
 
-/*
-Suggested name length:
-https://stackoverflow.com/questions/30485/what-is-a-reasonable-length-limit-on-person-name-fields
-*/
+--Suggested name length:https://stackoverflow.com/questions/30485/what-is-a-reasonable-length-limit-on-person-name-fields
 CREATE TABLE person (
-    id INTEGER UNSIGNED NOT NULL UNIQUE AUTO_INCREMENT,
+    id INTEGER UNIQUE NOT NULL AUTO_INCREMENT,
     forename VARCHAR(50) NOT NULL,
-    middle_initials VARCHAR (10),
+    middle_initials VARCHAR(10),
     surname VARCHAR(50) NOT NULL,
-    date_of_birth DATE NOT NULL CHECK (date_of_birth <= GetDate()),
+    date_of_birth DATE NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -22,7 +19,9 @@ Average word in English is 5 characters long, so 2500 allows a 500 words biograp
 this seems reasonable due to wikipedia summaries being roughly 500 words.
 */
 CREATE TABLE contributor (
+    person_id INTEGER NOT NULL UNIQUE,
     biography VARCHAR(2500),
+    PRIMARY KEY (person_id),
     FOREIGN KEY (person_id) REFERENCES person (id)
 );
 
@@ -34,8 +33,10 @@ Emails can have a maximum length of 320 as the user name can be 64 characters,
 the '@' symbol requires 1 character, and the domain can be 255 characters.
 */
 CREATE TABLE customer (
+    person_id INTEGER NOT NULL UNIQUE,
     email_address VARCHAR(320) UNIQUE CHECK (email_address LIKE '^[^@]+@[^@]+\.[^@]+$'),
-    FOREIGN KEY (person_id) REFERENCES person (id)
+    FOREIGN KEY (person_id) REFERENCES person (id),
+    PRIMARY KEY (person_id)
 );
 
 /*
@@ -45,9 +46,10 @@ https://stackoverflow.com/questions/20054770/regex-for-numbers-with-spaces-plus-
 Not all phone numbers are unique (i.e. house numbers)
 */
 CREATE TABLE phone_number (
+    customer_id INTEGER NOT NULL UNIQUE,
     phone_number VARCHAR(20) CHECK (phone_number LIKE '^\(?\+?[\d\(\-\s\)]+$'),
-    FOREIGN KEY (customer_id) REFERENCES customer (person_id),
-    PRIMARY KEY (phone_number, customer_id)
+    PRIMARY KEY (phone_number, customer_id),
+    FOREIGN KEY (customer_id) REFERENCES customer (person_id)
 );
 
 /*
@@ -71,15 +73,68 @@ CREATE TABLE publisher (
     country VARCHAR(75) NOT NULL,
     postcode VARCHAR(25),
     phone_number VARCHAR(20) CHECK (phone_number LIKE '^\(?\+?[\d\(\-\s\)]+$'),
-    established_date DATE NOT NULL CHECK (established_date <= GetDate()),
+    established_date DATE NOT NULL,
     PRIMARY KEY (name)
 );
 
 /**
 ISBN 13 max length: https://www.isbn-international.org/content/what-isbn
+
+Time would ideally be stored in a large integer type as seconds in case an audiobook
+is longer than a day.
+
+Chose LONGBLOB to allow for audio book files up to 4GB (e.g. the bible on audible
+which is 65 hours long)
 **/
 CREATE TABLE audiobook (
-    ISBN VARCHAR(17) UNIQUE NOT NULL CHECK(ISBN LIKE '^[0-9-]*$'),
+    ISBN VARCHAR(17) UNIQUE NOT NULL CHECK (ISBN LIKE '^[0-9-]*$'),
     title VARCHAR(250) NOT NULL,
+    narrator_id INTEGER UNIQUE NOT NULL,
+    running_time TIME NOT NULL CHECK (running_time > 0),
+    age_rating INTEGER CHECK (age_rating >= 0),
+    purchase_price FLOAT(10,2) NOT NULL DEFAULT 0,
+    publisher_name VARCHAR(200) UNIQUE NOT NULL,
+    published_date DATE NOT NULL,
+    audiofile LONGBLOB NOT NULL,
+    FOREIGN KEY (narrator_id) REFERENCES contributor (person_id),
+    FOREIGN KEY (publisher_name) REFERENCES publisher (name),
+    PRIMARY KEY (ISBN)
+);
 
+CREATE TABLE chapter (
+    ISBN VARCHAR(17) UNIQUE NOT NULL CHECK (ISBN LIKE '^[0-9-]*$'),
+    number INTEGER NOT NULL,
+    title VARCHAR(250) NOT NULL,
+    start TIME NOT NULL CHECK (start >= 0),
+    PRIMARY KEY (ISBN, number),
+    FOREIGN KEY (ISBN) REFERENCES audiobook (ISBN)
+);
+
+CREATE TABLE audiobook_authors (
+    contributor_id INTEGER NOT NULL UNIQUE,
+    ISBN VARCHAR(17) UNIQUE NOT NULL CHECK (ISBN LIKE '^[0-9-]*$'),
+    FOREIGN KEY (contributor_id) REFERENCES contributor (person_id),
+    FOREIGN KEY (ISBN) REFERENCES audiobook (ISBN),
+    PRIMARY KEY (contributor_id, ISBN)
+);
+
+CREATE TABLE audiobook_purchases (
+    customer_id INTEGER NOT NULL UNIQUE,
+    ISBN VARCHAR(17) UNIQUE NOT NULL CHECK (ISBN LIKE '^[0-9-]*$'),
+    purchase_date DATETIME NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer (person_id),
+    FOREIGN KEY (ISBN) references audiobook (ISBN),
+    PRIMARY KEY (customer_id, ISBN)
+);
+
+CREATE TABLE audiobook_reviews (
+    customer_id INTEGER NOT NULL UNIQUE,
+    ISBN VARCHAR(17) UNIQUE NOT NULL CHECK (ISBN LIKE '^[0-9-]*$'),
+    rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    title VARCHAR(250),
+    comment VARCHAR(2500),
+    verified Boolean NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer (person_id),
+    FOREIGN KEY (ISBN) REFERENCES audiobook (ISBN),
+    PRIMARY KEY (customer_id, ISBN)
 );
