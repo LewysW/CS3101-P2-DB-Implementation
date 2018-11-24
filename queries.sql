c9e70d670a731c705f2db41bd980361231aa6f67
locw <lewysward99@gmail.com>
2018-11-24 22:33:27 +0000

    First query completed

--- queries.sql
+++ queries.sql
@@ -1,14 +1,9 @@
 --Query 1
-SELECT person_id AS 'customer_id',
-(SELECT concat(forename, ' ', middle_initials, ' ', surname) FROM person WHERE customer_id = person_id) as 'full name',
-email_address, count(purchase_date) as 'Number of Purchases'
-FROM (customer NATURAL LEFT OUTER JOIN audiobook_purchases NATURAL LEFT OUTER JOIN audiobook) GROUP BY customer_id;
-
-SELECT * FROM audiobook_purchases;
-
-(SELECT audiobook_purchases.person_id as id, SUM(purchase_price) FROM audiobook, audiobook_purchases WHERE audiobook.ISBN = audiobook_purchases.ISBN GROUP BY id);
-
-SELECT * FROM audiobook;
+SELECT person_id AS customer_id, concat(surname, ' ', middle_initials, ' ', forename) as full_name, email_address, count(purchase_date) as books_purchased, COALESCE(SUM((SELECT SUM(purchase_price)
+FROM audiobook WHERE audiobook.ISBN = audiobook_purchases.ISBN)), 0.00) as total_spent
+FROM customer
+NATURAL LEFT JOIN audiobook_purchases
+NATURAL LEFT JOIN person GROUP BY person_id ORDER BY total_spent DESC, full_name ASC;
 --Query 2
 
 
